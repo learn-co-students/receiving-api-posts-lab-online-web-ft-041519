0d6c8f4af3a5d8a83da0c6b8169a61bacb432802
Alison Golden <alisong779@yahoo.com>
2019-08-15 15:16:17 -0400

    Done.

--- spec/views/invoices_index_view_spec.rb
+++ spec/views/invoices_index_view_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "invoices_index_view" do
 
     10.times do
       products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
-      Product.create(:name => products.sample, :price => Faker::Number.between(1,1500))
+      Product.create(:name => products.sample, :price => Faker::Number.between(from: 1, to: 1500))
     end
     5.times do
       Customer.create(:name => Faker::Name.name)
@@ -43,5 +43,4 @@ RSpec.describe "invoices_index_view" do
     render :template => "invoices/index.html.erb"
     expect(rendered).to match(/No Invoices/)
   end
-
-end
+end 
